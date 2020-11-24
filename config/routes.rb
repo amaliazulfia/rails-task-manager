d506c1b0d5fb30bbdadb4eb87bc3da210f9e0f15
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-11-24 16:27:01 +0800

    ex2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # get "tasks/new", to: "tasks#new",  as: :new_task
+  #  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  #  patch "tasks/:id", to: "tasks#update"
+
+  #  delete "tasks/:id", to: "tasks#destroy"
+
+
+  # get "tasks", to: "tasks#index", as: :tasks
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  
+  # post "tasks",  to: "tasks#create"
+
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
